e62a1ee9f1bfe2ccfa3e654bbd3a62496549e6e5
Marcos Renato Falconi <falconi.2050@gmail.com>
2017-11-13 08:43:57 -0200

    Teste

--- app/src/main/java/br/com/brotolegal/appinventario/LoginActivity.java
+++ app/src/main/java/br/com/brotolegal/appinventario/LoginActivity.java
@@ -92,6 +92,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+
+        //Teste de alteração para o git
     }
 
     private void populateAutoComplete() {
